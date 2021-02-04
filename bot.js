331e74951f511fcf4a1f13cebec14d98386ce4ed
Rebecca Turner <rbt@sent.as>
2021-02-04 18:14:16 -0500

    dumb features

--- bot.js
+++ bot.js
@@ -1,15 +1,17 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 client.on('ready', () => {
-    console.log('I am ready!');
-});
+    console.log('I am ready!')
+})
 
 client.on('message', message => {
     if (message.content.startsWith('-p')) {
-        message.reply('oh? you wanna play music? you wanna play a little music in my little discord server? you think you deserve to hear music? good. i love you.');
+        message.reply('oh? you wanna play music? you wanna play a little music in my little discord server? you think you deserve to hear music? good. i love you.')
+    } else if (message.content.indexOf('vibe check') !== -1) {
+        message.reply('great vibes!')
     }
-});
+})
 
-client.login(process.env.BOT_TOKEN); // BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN) // BOT_TOKEN is the Client Secret
