7400a13dd8a1093fda12f3b2c3b6573a80e7fa1e
Rebecca Turner <rbt@sent.as>
2021-02-04 18:26:39 -0500

    additional features (replying?)

--- bot.js
+++ bot.js
@@ -11,6 +11,8 @@ client.on('message', message => {
         message.reply('oh? you wanna play music? you wanna play a little music in my little discord server? you think you deserve to hear music? good. i love you.')
     } else if (message.content.indexOf('vibe check') !== -1) {
         message.reply('great vibes!')
+    } else if (message.mentions.users.first().id == client.user.id) {
+        message.reply("hey. can\'t talk right now, sorry. tbh i didn't even bother to read the message. smell ya later")
     }
 })
 
