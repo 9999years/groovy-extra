da206216919db924719c1ffeb3dd84a110481d9b
Rebecca Turner <rbt@sent.as>
2021-02-04 20:49:45 -0500

    fix build & ts stuff

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-const Discord = require('discord.js')
+// const Discord = require('discord.js')
+import * as Discord from 'discord.js'
 
 const client = new Discord.Client()
 
 client.on('ready', () => {
-    console.log('I am ready!')
+    console.log('Ready!')
+    client.user.setActivity("100 gecs", {type: "LISTENING"})
+        .catch(console.error)
 })
 
 client.on('message', message => {
@@ -17,27 +20,4 @@ client.on('message', message => {
 })
 
 client.login(process.env.BOT_TOKEN) // BOT_TOKEN is the Client Secret
-
-botUsers = {}
-
-client.guilds.cache
-    .each(guild => {
-        if (guild.available) {
-            guild.fetchIntegrations({includeApplications: true})
-                .then(integrations =>
-                    integrations.each(integration => {
-                        if (integration.application) {
-                            if (integration.application.bot) {
-                                botUsers[guild.id][integration.id][integration.application.id] = integration.application.bot.user
-                            }
-                        }
-                    }))
-        }
-    })
-
-client.fetchApplication()
-    .then(.user.setActivity("100 gecs", {
-    url: "https://github.com/9999years/groovy-extra/",
-    type: "LISTENING"
-})
 
